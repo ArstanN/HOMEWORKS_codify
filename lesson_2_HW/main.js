4bce45c03d14f5f427cfeb5e99e75c1ca9f40cc2
Aziza <kantemir@MacBook-Air-Aziza.local>
2023-03-27 16:50:39 +0600

    All_HW_File git status

---
+++ lesson_2_HW/main.js
@@ -0,0 +1,82 @@
+//! Задание 1
+
+// let year = 2023
+// if (year>2015) {
+//     alert(year)
+// }
+
+//! Задание 2
+// let num = 10
+// let num2 = 10
+// if (num - num2){
+//     alert(num-num2)
+// }
+
+
+//! Задание 3
+
+// let text = officialJavascsriptname = prompt("Какое официальное название JavaScript?")
+// if ("ECMAScript" === officialJavascsriptname) {
+//     alert("Верно!")
+// } else {
+//     alert("Не знаете? ECMAScript! ")
+// }
+
+
+//! Задание 4
+// let num = prompt('Введите число')
+// let num2 = prompt('Введите второе число')
+// if (num+num2 == 10) {
+//     alert(num + num2)
+// } else {
+//     console.log(num + num2)
+// }
+//! Задание 5
+
+// const vis = confirm ('Вам ест 18?')
+
+// vis 
+//     ? alert('Добро пожаловать!')
+//     : alert('Вход запрещен!')
+
+//! Задание 6
+
+// let str = "Javascript"
+
+// str === 'Javascript' 
+//     ? console.log('Frontend')
+//     : console.loh('Backend')
+
+//? Extra tasks
+
+//! Задание 1 
+// const month = +prompt('Введите число');
+
+// if (month === 1 || month === 2 || (month === 12) ) {
+//     alert('Зима');
+// } else if (month >= 3 && month <= 5) {
+//     alert('Весна');
+// } else if (month >= 6 && month <= 8) {
+//     alert('Лето');
+// } else if (month >= 9 && month <= 11) {
+//     alert('Осень');
+// } else {
+//     alert('Нету такого месяца')
+// }
+
+//! Задание 2
+
+// const name = prompt("Как тебя зовут?");
+
+// if (!name) {
+//     alert('Вы отменили действие')
+// } else {
+//     const confir = confirm ("Вы уверены?")
+
+//     if (confir) {
+//         alert(`Ваше имя - ${name}`)
+//     } else {
+//         alert("Попробуйте вспомнить")
+//     }
+// }
+
