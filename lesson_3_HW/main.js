25ab071e93e9aa297d8e9c2e88a8baaf639deb0a
Aziza <kantemir@MacBook-Air-Aziza.local>
2023-03-27 17:54:23 +0600

    HOMEWORK_SWITCH_CASE

---
+++ lesson_3_HW/main.js
@@ -0,0 +1,114 @@
+//! Задание 1
+
+// let num = prompt("Введите число");
+// let num2 = prompt("Введите второе число");
+// let sum = parseInt(num) + parseInt(num2);
+
+// switch (sum) {
+//     case 10:
+//         alert("10");
+//     break;
+//     case 20:
+//         alert("20");
+//     break;
+//     default:
+//         alert("default");
+// }
+
+//! Задание 2
+
+// let month = prompt("Введите месяц на русском")
+
+// switch(month) {
+//     case "Декабрь":
+//     case "Январь":
+//     case "Февраль":
+//         alert("Зима");
+//         break;
+//     case "Март":
+//     case "Апрель":
+//     case "Май":
+//         alert("Весна");
+//         break;
+//     case "Июнь":
+//     case "Июль":
+//     case "Август":
+//         alert("ЛЕто");
+//         break;
+//     case "Сентябрь":
+//     case "Октябрь":
+//     case "Ноябрь":
+//         alert("Осень");
+//         break;
+// }
+
+//! Задание 3
+
+// let age  = prompt("Введите возраст ученика")
+
+// if (age >= 6 && age <= 7) {
+//     alert("Первоклассник")
+// } else if (age >= 8 && age <= 10) {
+//     alert("Ученик начальной школф")
+// } else if (age >= 11 && age <= 13) {
+//     alert("Ученик средней школы")
+// } else if (age >= 14 && age <= 16) {
+//     alert("Ученик старшей школы")
+// } else {
+//     alert("Не ученик")
+// }
+
+//! Задание 3/2
+
+// let obj = {
+//     age: 18,
+//     firstName: "Arstan",
+//     lastName: "Nama"
+// }
+
+// console.log(obj.age)
+// console.log(obj.firstName)
+// console.log(obj.lastName)
+
+
+//! Задание 4
+
+// let arr = [1,2,3,4,5]
+
+// console.log(arr[2])
+
+//! Задание 5 
+
+// let obj = {}
+
+// obj.firstName = prompt("Введите имя")
+// obj.lastName = prompt("Введите фамилию")
+// obj.age = prompt("Введите возраст")
+
+// console.log(obj)
+
+//? Extra tasks
+
+//! 1
+// let object = {value:5}
+// let array = [5]
+
+// if (object.value === array[0]) {
+//     alert("ЗНАЧЕНИЯ РАВНЫ")
+// }
+
+
+//! 2
+
+// let arr = []
+
+// if (confirm("2+2=4?")) {
+//     arr.push("Верно")
+//     console.log(arr[0])
+// } else {
+//     let obj = {
+//         true:"Неверно"
+//     }
+//     console.log(obj.true)
+// }
+
