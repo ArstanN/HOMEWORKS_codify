973042eb46fe56e9fb55ca08ca2a27c7409b62c5
Aziza <kantemir@MacBook-Air-Aziza.local>
2023-04-02 18:16:40 +0600

    HW_METHODS

---
+++ lesson_5_HW/main.js
@@ -0,0 +1,74 @@
+//! Задание 1
+
+// const arr = [1,2,3]
+// const newArr = arr.concat([4,5,6])
+// console.log(newArr)
+
+
+//! Задание 2
+
+// const arr = [1,2,3]
+
+// console.log(arr.reverse())
+
+//! Задание 3
+
+// const arr = [1,2,3]
+// arr.push(4,5,6)
+// console.log(arr)
+
+//! Задание 4
+
+// const arr = [1,2,3]
+// arr.unshift(4,5,6)
+// console.log(arr)
+
+//! Задание 5
+
+// const arr = ['js', 'css', 'jq']
+
+// console.log(arr.slice(0,1))
+
+//! Задание 6
+
+// const arr = ['js', 'css', 'jq']
+
+// console.log(arr.slice(2))
+
+//! Задание 7
+
+// const arr = [1, 2, 3, 4, 5]
+// let newArr = arr.slice(0,3)
+// console.log(newArr)
+
+//! Задание 8
+
+// const arr = [1, 2, 3, 4, 5]
+// let newArr = arr.slice(3,5)
+// console.log(newArr)
+
+//! Задание 9
+
+// let arr = [1, 2, 3, 4, 5]
+// arr.splice(1, 2)
+// console.log(arr)
+
+//! Задание 10
+
+// const arr = [1, 2, 3, 4, 5]
+// const newArr = arr.splice(1, 3)
+// console.log(newArr)
+
+//! Задание 11
+
+// const arr = [1, 2, 3, 4, 5]
+// arr.splice(3, 0, 'a', 'b', 'c')
+// console.log(arr)
+
+//! Задание 12
+
+// let arr = [1, 2, 3, 4, 5]
+// arr.splice(1, 0, 'a', 'b')
+// arr.splice(6, 0, 'c')
+// arr.push('e')
+// console.log(arr)
