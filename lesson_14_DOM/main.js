780cfeae58ec5ec1ea4003345db06280d9c082e8
Aziza <kantemir@MacBook-Air-Aziza.local>
2023-06-06 22:21:50 +0600

    Dom HW

---
+++ lesson_14_DOM/main.js
@@ -0,0 +1,8 @@
+function submitForm() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let age = document.getElementById("age").value;
+  console.log("Имя: " + name);
+  console.log("Email: " + email);
+  console.log("Возраст: " + age);
+}
